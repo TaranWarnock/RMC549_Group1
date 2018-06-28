4e649771e4d809c5492210bfa20a7dd6649185c0
curtispuetz <puetzcurtis@gmail.com>
2018-06-27 21:07:03 -0400

    small changes

--- talking_to_sensors/talking_to_gps/talking_to_gps.ino
+++ talking_to_sensors/talking_to_gps/talking_to_gps.ino
@@ -1,13 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
-  Serial1.begin(9600);
-
+  Serial1.begin(4800);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (Serial1.available()) {
-    char gps_string = Serial1.read();
-    Serial.write(gps_string);
+    Serial.write(Serial1.read());
   }
 }
