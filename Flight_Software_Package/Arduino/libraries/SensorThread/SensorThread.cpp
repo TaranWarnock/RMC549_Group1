840903ae7046b0818e1d0abf925c3d6ae354cdf7
Taran Warnock (tww275) <taran.warnock@usask.ca>
2018-06-29 14:37:46 -0400

    Switched to using Strings for sensor data; can convert from String to char array in the arduino manager class if necessary

--- Flight_Software_Package/Arduino/libraries/SensorThread/SensorThread.cpp
+++ Flight_Software_Package/Arduino/libraries/SensorThread/SensorThread.cpp
@@ -1,11 +1,14 @@
 #include "SensorThread.h"
 
 void SensorThread::run() {
-    callback();
+    readFromSensor();
 
     runned();
 }
 
-void GPSSensorThread::callback() {
+// Implement the code for a single GPS reading event here
+void GPSSensorThread::readFromSensor() {
     // read from sensor and store in buffer
+    Serial.println("GPS Sensor Class");
+    sensorData = "GPD, sensor data here";
 }
