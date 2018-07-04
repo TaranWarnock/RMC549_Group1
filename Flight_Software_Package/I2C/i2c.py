c2f912df669093d7fdf75b65848f5dc1cc2d0d85
lukasfehr <fehr.lukas@gmail.com>
2018-07-04 17:55:04 -0400

    added IMU package (won't need for final software)

---
+++ Flight_Software_Package/I2C/i2c.py
@@ -0,0 +1,9 @@
+from Common.FSW_Common import *
+
+
+class I2C_Photosensor(FlightSoftwareParent):
+    def __init__(self, logging_object: Logger):
+        super().__init__("I2CPhotosensor", logging_object)
+
+
+
