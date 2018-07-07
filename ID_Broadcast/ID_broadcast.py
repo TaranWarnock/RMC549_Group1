409ba492753706783d6a283be60f8a5ebe5e45d8
DanielLetros <dsletros@hotmail.com>
2018-07-07 20:21:39 +0000

    flight checkout code update

--- ID_Broadcast/ID_broadcast.py
+++ ID_Broadcast/ID_broadcast.py
@@ -81,15 +81,18 @@ while True:
     # last_data = read_last_line_in_data_log(get_newest_data_file(path_to_log_files))
     # last_note = read_last_line_in_data_log(get_newest_notification_file(path_to_log_files))
 
-    note_message = "%s<<%s" % (socket.gethostname(), read_last_line_in_data_log(new_log_list[0][1]))
+    try:
+        note_message = "%s<<%s" % (socket.gethostname(), read_last_line_in_data_log(new_log_list[0][1]))
 
-    sock.sendto(bytes(note_message, 'utf-8'), ('<broadcast>', 55555))
-    time.sleep(0.3)
+        sock.sendto(bytes(note_message, 'utf-8'), ('<broadcast>', 55555))
+        time.sleep(0.3)
 
-    data_message = "%s<<%s" % (socket.gethostname(), read_last_line_in_data_log(new_log_list[1][1]))
+        data_message = "%s<<%s" % (socket.gethostname(), read_last_line_in_data_log(new_log_list[1][1]))
 
-    sock.sendto(bytes(data_message, 'utf-8'), ('<broadcast>', 55555))
-    time.sleep(0.3)
+        sock.sendto(bytes(data_message, 'utf-8'), ('<broadcast>', 55555))
+        time.sleep(0.3)
+    except:
+        pass
 
     sock.close()
     time.sleep(0.2)
