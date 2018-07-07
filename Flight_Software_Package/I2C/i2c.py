726f5c02cecf7e9848e740be7689ee7959d9dbf3
DanielLetros <dsletros@hotmail.com>
2018-07-07 17:30:56 +0000

    flight checkout code update

--- Flight_Software_Package/I2C/i2c.py
+++ Flight_Software_Package/I2C/i2c.py
@@ -24,35 +24,42 @@ class I2C_Photosensor(object):
     def _initialize(self) -> None:
         """ Initializing commands. Taken from TSL2561.py script. """
         if self.sensor_is_valid:
-            # Get I2C bus
-            self._bus = smbus.SMBus(1)
+            try:
+                # Get I2C bus
+                self._bus = smbus.SMBus(1)
 
-            # TSL2561 address, 0x39(57)
-            # Select control register, 0x00(00) with command register, 0x80(128)
-            #		0x03(03)	Power ON mode
-            self._bus.write_byte_data(self._addr, 0x00 | 0x80, 0x03)
-            # TSL2561 address, 0x39(57)
-            # Select timing register, 0x01(01) with command register, 0x80(128)
-            #		0x02(02)	Nominal integration time = 402ms
-            self._bus.write_byte_data(self._addr, 0x01 | 0x80, 0x02)
+                # TSL2561 address, 0x39(57)
+                # Select control register, 0x00(00) with command register, 0x80(128)
+                #		0x03(03)	Power ON mode
+                self._bus.write_byte_data(self._addr, 0x00 | 0x80, 0x03)
+                # TSL2561 address, 0x39(57)
+                # Select timing register, 0x01(01) with command register, 0x80(128)
+                #		0x02(02)	Nominal integration time = 402ms
+                self._bus.write_byte_data(self._addr, 0x01 | 0x80, 0x02)
+            except:
+                self.sensor_is_valid = False
 
     def _get_data(self):
         """ Read the data from the sensor. Returns lux values (VIS, IR). Taken from TSL2561.py script. """
-        if self.sensor_is_valid:
-            # Read data back from 0x0C(12) with command register, 0x80(128), 2 bytes
-            # ch0 LSB, ch0 MSB
-            data = self._bus.read_i2c_block_data(self._addr, 0x0C | 0x80, 2)
+        try:
+            if self.sensor_is_valid:
+                # Read data back from 0x0C(12) with command register, 0x80(128), 2 bytes
+                # ch0 LSB, ch0 MSB
+                data = self._bus.read_i2c_block_data(self._addr, 0x0C | 0x80, 2)
 
-            # Read data back from 0x0E(14) with command register, 0x80(128), 2 bytes
-            # ch1 LSB, ch1 MSB
-            data1 = self._bus.read_i2c_block_data(self._addr, 0x0E | 0x80, 2)
+                # Read data back from 0x0E(14) with command register, 0x80(128), 2 bytes
+                # ch1 LSB, ch1 MSB
+                data1 = self._bus.read_i2c_block_data(self._addr, 0x0E | 0x80, 2)
 
-            # Convert the data
-            ch0 = data[1] * 256 + data[0]
-            ch1 = data1[1] * 256 + data1[0]
+                # Convert the data
+                ch0 = data[1] * 256 + data[0]
+                ch1 = data1[1] * 256 + data1[0]
 
-            return ch0 - ch1, ch1
-        else:
+                return ch0 - ch1, ch1
+            else:
+                return None
+        except:
+            self.sensor_is_valid = False
             return None
 
 
