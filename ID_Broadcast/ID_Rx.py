7332131020036ab8b4229b2c1cb77311a89f1cec
DanielLetros <dsletros@hotmail.com>
2018-07-11 18:13:53 +0000

    Update to ID Rx

--- ID_Broadcast/ID_Rx.py
+++ ID_Broadcast/ID_Rx.py
@@ -2,6 +2,9 @@ import socket
 import time
 import datetime
 
+Look_At_Rocky    = True
+Look_At_MajorTom = False
+
 UDP_IP   = "0.0.0.0"
 UDP_PORT = 55555
 
@@ -11,5 +14,10 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(32768)
-    print(str(datetime.datetime.utcnow().strftime("%Y%m%d_%H:%M:%S.%f")) + "::" + str(addr[0]) + "::" + str(UDP_PORT) + " << " + str(data.decode('utf-8')))
+    line = str(datetime.datetime.utcnow().strftime("%Y%m%d_%H:%M:%S.%f")) + "::" + \
+           str(addr[0]) + "::" + str(UDP_PORT) + " << " + str(data.decode('utf-8'))
+    if line.split(" << ")[1] == "MajorTom" and Look_At_MajorTom:
+        print(line)
+    if line.split(" << ")[1] == "Rocky" and Look_At_Rocky:
+        print(line)
     time.sleep(0.1)
