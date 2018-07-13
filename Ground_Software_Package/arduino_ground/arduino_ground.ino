da6fa2dc61d80710f793eb573cbcf652bccad27e
curtispuetz <36495179+curtispuetz@users.noreply.github.com>
2018-07-12 22:29:12 -0400

    Add files via upload

--- Ground_Software_Package/arduino_ground/arduino_ground.ino
+++ Ground_Software_Package/arduino_ground/arduino_ground.ino
@@ -1,3 +1,6 @@
+// Author: Curtis Puetz
+// Adapted from: https://learn.adafruit.com/adafruit-rfm69hcw-and-rfm96-rfm95-rfm98-lora-packet-padio-breakouts/rfm9x-test
+
 #include <SPI.h>
 #include <RH_RF95.h>
 
@@ -23,6 +26,8 @@ void setup() {
   Serial.begin(9600);
   delay(100);
 
+  Serial.println("Arduino LoRa RX Test!");
+  
   // manual reset
   digitalWrite(RFM95_RST, LOW);
   delay(10);
@@ -33,14 +38,17 @@ void setup() {
     Serial.println("LoRa radio init failed");
     while (1);
   }
+  Serial.println("LoRa radio init OK!");
 
   // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
   if (!rf95.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
     while (1);
   }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
 
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
   // The default transmitter power is 13dBm, using PA_BOOST.
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
   // you can set transmitter powers from 5 to 23 dBm:
@@ -48,7 +56,7 @@ void setup() {
 }
 
 void loop(){
-  // check if message from the LoRa is available
+	// check if there is communication from the LoRa device (i.e. check if downlinked data has been recieved downlinked data is recieved)
   if (rf95.available()){
     // Should be a message for us now   
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
@@ -57,30 +65,32 @@ void loop(){
     if (rf95.recv(buf, &len)){
       digitalWrite(LED, HIGH);
       //RH_RF95::printBuffer("Received: ", buf, len);
-      delay(10); // this line is concerting, the commented line above is better because it ensures the buf is available before use
-      // print the comma separated return file
+      // wait for a sufficient amount of time for the full data string to be recieved
+      delay(5); 
+      // print the recieved data on the serial port with the RSSI signal strength appended to it
       Serial.print((char*)buf);
       Serial.print(",");
-      // print the RSSI
       Serial.println(rf95.lastRssi(), DEC);
     }
     else{
       Serial.println("Receive failed");
     }
   }
-
-  // check if there is a command available to be uplinked
+  // check if commands from the main ground station computer have been recieved (for uplink to baloon)
   if (Serial.available() > 0){
   String data = "";
+  // read the complete uplink command
   while (Serial.available()>0){
     data.concat((char)Serial.read());
   }
   int len_data = data.length() + 1;
   char data1[len_data];
   data.toCharArray(data1, len_data);
+  // send the uplink command
   rf95.send((uint8_t*)data1, len_data);
   rf95.waitPacketSent();
   //Serial.println("Sent a reply");
+  // write the LED low for visual confirmation that the command has been sent
   digitalWrite(LED, LOW);
   }
 }
